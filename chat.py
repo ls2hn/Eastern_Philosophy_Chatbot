ddb717dd9fd320d78421381d21e81b8f8789081c
ls2hn <s2hnsu@gmail.com>
2026-01-29 08:28:58 +0900

    feat: update sidebar text and add Oriental Classics DB link

--- chat.py
+++ chat.py
@@ -14,27 +14,37 @@ if "NEO4J_URI" in st.secrets:
     os.environ["NEO4J_PASSWORD"] = st.secrets["NEO4J_PASSWORD"]
 
 from llm import get_ai_response
-APK_PAGE_URL = "https://lily-naranja-3e1.notion.site/On-Go-2e5a6169acfe80adac9bcde2e1e5b38e"
+APK_PAGE_URL = "https://lily-naranja-3e1.notion.site/2f6a6169acfe80919bd6f40dbfa1a5ee?pvs=74"
+ANALECTS_URL = "https://db.cyberseodang.or.kr/front/alphaList/BookMain.do?tab=tab1_02&bnCode=jti_1h0301&titleId=C2"
+MENCIUS_URL = "https://db.cyberseodang.or.kr/front/alphaList/BookMain.do?tab=tab1_02&bnCode=jti_1h0601&titleId=C2"
+CHANGES_URL = "https://db.cyberseodang.or.kr/front/alphaList/BookMain.do?tab=tab1_02&bnCode=jti_1a0201&titleId=C1"
+LEARNING_URL = "https://db.cyberseodang.or.kr/front/alphaList/BookMain.do?tab=tab1_02&bnCode=jti_1h0801&titleId=C1"
+MEAN_URL = "https://db.cyberseodang.or.kr/front/alphaList/BookMain.do?tab=tab1_02&bnCode=jti_1h1001&titleId=C2"
+
 st.set_page_config(page_title="Confucian Chatbot", page_icon="📜") 
 
 with st.sidebar:
-    st.header("溫故(On-Go) 모바일 앱❕")
+    st.header("溫故(On-Go) 모바일 앱❗")
     st.markdown("온고지신(溫故知新)의 정신을 바탕으로 한 유학 기반 상담봇.")
     st.write("이 상담봇은 웹에서 바로 사용 가능하며, Android 기기에서는 APK로 설치해 모바일 앱으로도 이용할 수 있습니다.")
+    st.write("온고가 잘하는 것: 'Graph-RAG를 활용하여 신뢰도 높은 철학적 답변 생성'")
     st.markdown(f"📱 **[다운로드 페이지로 이동]({APK_PAGE_URL})**")
     st.markdown(
-            """
-- APK는 이 링크에서만 다운로드하세요.  
-- 압축을 풀고난 후 앱 설치 시 Android 설정에서 **‘알 수 없는 앱 설치’ 허용**이 필요합니다.
-- 인터넷 연결이 필요하며, 입력 내용은 답변 생성을 위해 서버/외부 API로 전송됩니다.  
-  **민감한 개인정보(주민번호/계좌/비밀번호 등)는 입력하지 마세요.**  
-- 전문 판단이 필요한 사안은 **전문가 상담을 권장**합니다.
+            """  
+- 이 모바일 앱은 인터넷 연결이 필요하며, 입력 내용은 답변 생성을 위해 서버/외부 API로 전송됩니다.  
+  **민감한 개인정보(주민번호/계좌/비밀번호 등)는 입력을 삼가주세요.**  
 """
     )
+    st.header("동양고전DB 사이트에서 유학경전 더 읽어보기❕")
+    st.markdown(f"**[논어]({ANALECTS_URL})**")
+    st.markdown(f"**[맹자]({MENCIUS_URL})**")
+    st.markdown(f"**[주역]({CHANGES_URL})**")
+    st.markdown(f"**[대학]({LEARNING_URL})**")
+    st.markdown(f"**[중용]({MEAN_URL})**")
 
 
 
-st.title("📜성찰을 돕는 유학 기반 상담봇: 溫故(On-Go)")
+st.title("📜일상의 성찰을 돕는 유학(儒學)기반 상담 챗봇: 溫故(On-Go)")
 st.caption("유학의 지혜를 빌려 당신의 일상을 함께 성찰합니다. 고민이 있나요?")
 
 load_dotenv()
